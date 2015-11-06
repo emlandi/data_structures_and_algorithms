5e46b1fd745ec3dc8a1f52e4922af5961dd2a073
Emily Landi <emily.landi@gmail.com>
2015-11-05 22:37:27 -0800

    finished testing

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 var arrays = require(__dirname + '/lib/arrays');
 var unique = require(__dirname + '/lib/unique');
 var frequency = require(__dirname + '/lib/frequency');
+
+//remove & return the last item
+var testPop = [1, 2, 3, 4];
+console.log(arrays.pop(testPop));
+
+//add item to the end
+var testPush = [1, 2, 3, 4];
+var itemPush = 5;
+console.log(arrays.push(testPush, itemPush));
+
+//remove & return the ﬁrst item
+var testShift = [1, 2, 3, 4];
+console.log(arrays.shift(testShift));
+
+//add item to the beginning
+var testUns = [1, 2, 3, 4];
+var itemUns = 0;
+console.log(arrays.unshift(testUns, itemUns));
+
+//return array copy without duplicates
+var testUnique = [1, 2, 2, 3, 3, 3];
+console.log(unique.unique(testUnique));
+
+//return the most common letter
+var testFreq = ['apple', 'banana'];
+console.log(frequency.frequency(testFreq));
+
+
--- test/test.js
+++ test/test.js
@@ -1,20 +1,48 @@
 var expect = require('chai').expect;
-var index = require(__dirname + '/../index');
+var arrays = require(__dirname + '/../lib/arrays');
+var unique = require(__dirname + '/../lib/unique');
+var frequency = require(__dirname + '/../lib/frequency');
 
-describe('the pop function', function() {
-  before(function() {
-    var pop = pop();
-  });
-  it('should remove an item from the array', function(){
-    expect(pop.array.length).to.eql(array.length-1);
+describe('the POP function', function() {
+  it('should remove & return the last item of array', function(){
+    var test = [1, 2, 3, 4];
+    expect(arrays.pop(test)).to.eql(4);
   });
 });
 
-describe('the push function', function() {
-  before(function() {
-    var push = push();
+describe('the PUSH function', function() {
+  it('should add item to the end of array', function(){
+    var test = [1, 2, 3, 4];
+    var item = 5;
+    expect(arrays.push(test, item)).to.eql([1,2,3,4,5]);
   });
-  it('should add an item to the array', function(){
-    expect(push.array.length).to.eql(array.length+1);
+});
+
+describe('the SHIFT function', function() {
+  it('should remove & return the ﬁrst item of array', function(){
+    var test = [1, 2, 3, 4];
+    expect(arrays.shift(test)).to.eql(1);
+  });
+});
+
+describe('the UNSHIFT function', function() {
+  it('should add item to the beginning of array', function(){
+    var test = [1, 2, 3, 4];
+    var item = 0;
+    expect(arrays.unshift(test, item)).to.eql([0, 1, 2, 3, 4]);
+  });
+});
+
+describe('the UNIQUE function', function() {
+  it('should return array copy without duplicates', function(){
+    var test = [1, 2, 2, 3, 3, 3];
+    expect(unique.unique(test)).to.eql([1, 2, 3]);
+  });
+});
+
+describe('the FREQUENCY function', function() {
+  it('should return the most common letter', function(){
+    var test = ['apple', 'banana'];
+    expect(frequency.frequency(test)).to.eql('a');
   });
 });
